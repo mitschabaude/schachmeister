db711e818ef9478bedd74f0bada7cfe41222ac6d
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2025-10-18 19:32:12 +0200

    definiere typen fuer brett

---
+++ src/schach/types.ts
@@ -0,0 +1,33 @@
+export type { Figur, Brett };
+
+type FigurArt = "bauer" | "laeufer" | "pferd" | "turm" | "dame" | "koenig";
+type Farbe = "w" | "b";
+
+type Figur = {
+  art: FigurArt;
+  farbe: Farbe;
+};
+
+type Feld = Figur | undefined;
+
+/**
+ * Das Schachbrett ist ein 8x8 Array von Figuren oder leeren Feldern.
+ * Wir verwenden `undefined`, um ein leeres Feld darzustellen.
+ *
+ * Beispiel:
+ * [ [undefined, undefined, ...], // 8 Felder in der ersten Reihe
+ *   [undefined, { art: "bauer", farbe: "w" }, ...], // 8 Felder in der zweiten Reihe
+ *   ... // Reihen 3 bis 7
+ *   [ { art: "koenig", farbe: "b" }, undefined, ...]  // 8 Felder in der achten Reihe
+ * ]
+ */
+type Brett = [
+  [Feld, Feld, Feld, Feld, Feld, Feld, Feld, Feld],
+  [Feld, Feld, Feld, Feld, Feld, Feld, Feld, Feld],
+  [Feld, Feld, Feld, Feld, Feld, Feld, Feld, Feld],
+  [Feld, Feld, Feld, Feld, Feld, Feld, Feld, Feld],
+  [Feld, Feld, Feld, Feld, Feld, Feld, Feld, Feld],
+  [Feld, Feld, Feld, Feld, Feld, Feld, Feld, Feld],
+  [Feld, Feld, Feld, Feld, Feld, Feld, Feld, Feld],
+  [Feld, Feld, Feld, Feld, Feld, Feld, Feld, Feld]
+];
