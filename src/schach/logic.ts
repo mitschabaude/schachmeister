9e1ef7a8544cf94e242f6b594699f041a9a02853
Jakob Mitscha-Baude <jakob.mitschabaude@gmail.com>
2026-02-08 18:39:22 +0100

    angefangen mit bauer

--- src/schach/logic.ts
+++ src/schach/logic.ts
@@ -1,10 +1,11 @@
-import type { Status, Zug, Position, Figur } from "./types";
+import type { Status, Zug, Position, Figur, Brett } from "./types";
 
 export { istKorrekterZug, zugAnwenden };
 
 function istKorrekterZug(zug: Zug, status: Status): boolean {
   // TODO
   if (status.amZug !== zug.figur.farbe) return false;
+  if (zug.figur.art === "bauer") return istKorrekterBauernZug(zug, status.brett);
   return true;
 }
 
@@ -19,4 +20,18 @@ function zugAnwenden(zug: Zug, { ...status }: Status): Status {
 function ändereAmZug(status: Status) {
   if (status.amZug === "b") status.amZug = "w"
   else status.amZug = "b"
+}
+
+function istKorrekterBauernZug(zug: Zug, brett: Brett): boolean {
+  if (zug.von.reihe + 1 == zug.nach.reihe) {
+    if (zug.figur.farbe == "w") { }
+  }
+  if (zug.von.reihe - 1 == zug.nach.reihe) {
+    if (zug.figur.farbe == "b") {
+      if (zug.von.spalte === zug.nach.spalte) {
+        if (brett[zug.von.reihe + 1]![zug.von.spalte]) return true
+      }
+      if (zug.nach.spalte === zug.von.spalte + 1 || zug.nach.spalte === zug.von.spalte - 1) { }
+    }
+  }
 }
