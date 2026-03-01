d5f218bb8c57c6fbef1f812b42f11f2edccc3a66
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2026-03-01 20:51:37 +0100

    fix

--- src/schach/logic.ts
+++ src/schach/logic.ts
@@ -18,7 +18,7 @@ function istKorrekterZug(zug: Zug, status: Status): boolean {
   return true;
 }
 
-function zugAnwenden(zug: Zug, status: Status): Status {
+function zugAnwenden(zug: Zug, { ...status }: Status): Status {
   console.log("Wir machen einen zug!", zug);
   if (zug.figur.art == "bauer") {
     if (zug.figur.farbe == "b" && zug.nach.reihe == 7) status.bauernUmwandlung = zug.nach;
